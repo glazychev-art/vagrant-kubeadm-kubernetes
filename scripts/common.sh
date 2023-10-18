a9f3fab7138721bad079004f6cfddf4e2af25f80
Artem Glazychev <artem.glazychev@xored.com>
2023-10-18 14:38:33 +0700

    opensuse setup
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,82 +2,93 @@
 #
 # Common setup for all servers (Control Plane and Nodes)
 
-set -euxo pipefail
+#sudo zypper rr http://download.opensuse.org/distribution/leap/15.2/repo/oss/
+#sudo zypper rr http://download.opensuse.org/distribution/leap/15.2/repo/non-oss/
+#sudo zypper rr http://download.opensuse.org/update/leap/15.2/oss/
+#sudo zypper rr http://download.opensuse.org/update/leap/15.2/non-oss/
+#
+#sudo zypper ar --refresh https://download.opensuse.org/distribution/leap/15.4/repo/oss/ leap154-oss
+#sudo zypper ar --refresh https://download.opensuse.org/distribution/leap/15.4/repo/non-oss/ leap154-non-oss
+#sudo zypper ar --refresh https://download.opensuse.org/update/leap/15.4/oss/ leap154-update-oss
+#sudo zypper ar --refresh https://download.opensuse.org/update/leap/15.4/non-oss/ leap154-update-non-oss
+#sudo zypper --gpg-auto-import-keys refresh
 
-# Variable Declaration
 
-# DNS Setting
-if [ ! -d /etc/systemd/resolved.conf.d ]; then
-	sudo mkdir /etc/systemd/resolved.conf.d/
-fi
-cat <<EOF | sudo tee /etc/systemd/resolved.conf.d/dns_servers.conf
-[Resolve]
-DNS=${DNS_SERVERS}
-EOF
+sudo zypper update
+sudo zypper refresh
 
-sudo systemctl restart systemd-resolved
-
-# disable swap
+#ensure the swap is disable
 sudo swapoff -a
-
-# keeps the swap off during reboot
 (crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
-sudo apt-get update -y
-# Install CRI-O Runtime
 
-VERSION="$(echo ${KUBERNETES_VERSION} | grep -oE '[0-9]+\.[0-9]+')"
+sudo zypper install -y docker
+#enable the docker service started on boot
+sudo systemctl enable docker
+sudo systemctl start docker
 
-# Create the .conf file to load the modules at bootup
-cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
-overlay
-br_netfilter
-EOF
+sudo docker ps
 
-sudo modprobe overlay
-sudo modprobe br_netfilter
+sudo zypper install -y jq
 
-# Set up required sysctl params, these persist across reboots.
-cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-EOF
+#sudo zypper addrepo --refresh https://download.opensuse.org/repositories/system:/snappy/openSUSE_Leap_15.5 snappy
+#sudo zypper --gpg-auto-import-keys refresh
+#sudo zypper dup --from snappy
+#sudo zypper install -y snapd
+#sudo systemctl enable --now snapd
+#sudo systemctl enable --now snapd.apparmor
+#
+#sudo snap install kubelet --classic
+#sudo snap install kubeadm --classic
+#sudo snap install kubectl --classic
 
-sudo sysctl --system
+sudo zypper install -y conntrack-tools
+sudo zypper install -y cri-o
+sudo systemctl enable crio.service
 
-cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
-EOF
-cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
-deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
-EOF
-
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
-
-sudo apt-get update
-sudo apt-get install cri-o cri-o-runc -y
-
-cat >> /etc/default/crio << EOF
-${ENVIRONMENT}
-EOF
-sudo systemctl daemon-reload
-sudo systemctl enable crio --now
-
-echo "CRI runtime installed successfully"
-
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update -y
-sudo apt-get install -y kubelet="$KUBERNETES_VERSION" kubectl="$KUBERNETES_VERSION" kubeadm="$KUBERNETES_VERSION"
-sudo apt-get update -y
-sudo apt-get install -y jq
-
-local_ip="$(ip --json a s | jq -r '.[] | if .ifname == "eth1" then .addr_info[] | if .family == "inet" then .local else empty end else empty end')"
-cat > /etc/default/kubelet << EOF
+export local_ip="$(ip --json a s | jq -r '.[] | if .ifname == "eth1" then .addr_info[] | if .family == "inet" then .local else empty end else empty end')"
+sudo -E bash -c 'cat > /etc/default/kubelet << EOF
 KUBELET_EXTRA_ARGS=--node-ip=$local_ip
 ${ENVIRONMENT}
-EOF
+EOF'
+
+sudo zypper install -y kubernetes1.20-kubelet
+sudo zypper install -y kubernetes1.20-kubeadm
+sudo zypper install -y kubernetes1.20-client
+
+
+#sudo zypper ar --refresh https://download.opensuse.org/tumbleweed/repo/oss/ tumbleweed-oss
+#sudo zypper --gpg-auto-import-keys refresh
+#
+#sudo zypper install -y conntrack-tools
+#sudo zypper install -y containerd
+#sudo zypper install -y cri-o
+#sudo zypper install -y --no-recommends kubernetes1.28-kubelet
+#sudo zypper install -y --no-recommends kubernetes1.28-kubeadm
+#sudo zypper install -y --no-recommends kubernetes1.28-client
+
+
+
+
+
+
+#enable the kubelet service started on boot
+sudo systemctl enable kubelet.service
+
+
+
+
+
+
+#sudo zypper install -y firewalld
+#
+#sudo systemctl enable firewalld
+#sudo systemctl start firewalld
+
+#sudo firewall-cmd --query-port=6443/tcp
+
+#add port in the firewall rules
+#sudo firewall-cmd --permanent --zone=public --add-port=6443/tcp
+#
+#sudo systemctl reload firewalld
+
+
--- scripts/master.sh
+++ scripts/master.sh
@@ -2,23 +2,17 @@
 #
 # Setup for Control Plane (Master) servers
 
-set -euxo pipefail
+sudo env "PATH=$PATH" kubeadm init --kubernetes-version=1.20.13 \
+    --apiserver-advertise-address=$CONTROL_IP \
+    --apiserver-cert-extra-sans=$CONTROL_IP \
+    --image-repository registry.aliyuncs.com/google_containers \
+    --service-cidr=$SERVICE_CIDR \
+    --pod-network-cidr=$POD_CIDR \
+    --node-name "$NODENAME" --ignore-preflight-errors Swap
 
-NODENAME=$(hostname -s)
-
-sudo kubeadm config images pull
-
-echo "Preflight Check Passed: Downloaded All Required Images"
-
-sudo kubeadm init --apiserver-advertise-address=$CONTROL_IP --apiserver-cert-extra-sans=$CONTROL_IP --pod-network-cidr=$POD_CIDR --service-cidr=$SERVICE_CIDR --node-name "$NODENAME" --ignore-preflight-errors Swap
-
-mkdir -p "$HOME"/.kube
-sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
-sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
-
-# Save Configs to shared /Vagrant location
-
-# For Vagrant re-runs, check if there is existing configs in the location and delete it for saving new configuration.
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 config_path="/vagrant/configs"
 
@@ -34,11 +28,56 @@ chmod +x $config_path/join.sh
 
 kubeadm token create --print-join-command > $config_path/join.sh
 
-# Install Calico Network Plugin
 
-curl https://raw.githubusercontent.com/projectcalico/calico/v${CALICO_VERSION}/manifests/calico.yaml -O
+kubectl create -f https://projectcalico.docs.tigera.io/archive/v3.22/manifests/tigera-operator.yaml
+curl https://projectcalico.docs.tigera.io/archive/v3.22/manifests/custom-resources.yaml -O
+sed -i -e 's|192.168.0.0/16|172.16.0.0/16|g' custom-resources.yaml
+kubectl apply -f custom-resources.yaml
 
-kubectl apply -f calico.yaml
+
+
+
+## Install Calico Network Plugin
+#cat > patch.yaml <<EOF
+#---
+#kind: DaemonSet
+#apiVersion: apps/v1
+#metadata:
+#  name: calico-node
+#  namespace: kube-system
+#  labels:
+#    k8s-app: calico-node
+#spec:
+#  template:
+#    spec:
+#      containers:
+#        - name: calico-node
+#          env:
+#            - name: IP_AUTODETECTION_METHOD
+#              value: "interface=eth1"
+#EOF
+#
+#cat > kustomization.yaml <<EOF
+#---
+#apiVersion: kustomize.config.k8s.io/v1beta1
+#kind: Kustomization
+#
+#resources:
+#  - https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/calico.yaml
+#
+#patchesStrategicMerge:
+#  - patch.yaml
+#EOF
+#
+#kubectl apply -k .
+
+
+
+
+#curl https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml -O
+#sed -i -e 's|10.244.0.0/16|172.16.0.0/16|g' kube-flannel.yml
+#kubectl apply -f kube-flannel.yml
+
 
 sudo -i -u vagrant bash << EOF
 whoami
@@ -47,7 +86,4 @@ sudo cp -i $config_path/config /home/vagrant/.kube/
 sudo chown 1000:1000 /home/vagrant/.kube/config
 EOF
 
-# Install Metrics Server
-
-kubectl apply -f https://raw.githubusercontent.com/techiescamp/kubeadm-scripts/main/manifests/metrics-server.yaml
-
+kubectl get pods --all-namespaces
